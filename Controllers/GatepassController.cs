70b918e08da8269a339004876bfe33bfe04d1a3a
Gray <azhadolfo1@gmail.com>
2023-09-06 17:01:01 +0800

    testing na nga to

---
+++ Controllers/GatepassController.cs
@@ -0,0 +1,19 @@
+﻿using DocumentManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Document_Management.Controllers
+{
+    public class GatepassController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //RequestGatepass
+        public IActionResult RequestGatepass()
+        {
+            return View();
+        }
+    }
+}
