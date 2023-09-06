907955ac527b1c07dde3a69aaed8d538346dbd7e
Gray <azhadolfo1@gmail.com>
2023-09-06 10:17:43 +0800

    testing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace DocumentManagement.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //Gatepass
+        public IActionResult Gatepass()
+        {
+            return View();
+        }
     }
 }
