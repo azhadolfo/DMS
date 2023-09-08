bc2c5e2064e4ff29bf33337e42e55690b1b416e3
azhadolfo <azhadolfo1@gmail.com>
2023-09-08 09:29:09 +0800

    create filedocument model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace Document_Management.Data
         }
 
         public DbSet<Register> Account { get; set; }
+        public DbSet<FileDocument> FileDocuments { get;}
 
         
     }
---
+++ Models/FileDocument.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Document_Management.Models
+{
+    public class FileDocument
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "File Name")]
+        public string Name { get; set; } = null!;
+        [Required]
+        [Display(Name = "File Location")]
+        public string Location { get; set; } = null!;
+        [Required]
+        public string Department { get; set; } = null!;
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        [Display(Name = "Date Uploaded")]
+        public DateTime DateUploaded { get; set; }
+        [Required]
+        public string Username { get; set; } = null!;
+    }
+}
