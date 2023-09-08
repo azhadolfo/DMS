c7c8b0aa09bd8e08df54e96b5f79e57f94bb886a
Gray <azhadolfo1@gmail.com>
2023-09-08 09:19:41 +0800

    Create RequestGatepass

---
+++ Migrations/20230908011339_Update DateTime in TableGatepass.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Document_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDateTimeinTableGatepass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ScheduleDate",
+                table: "Gatepass", // Replace with your actual table name
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
