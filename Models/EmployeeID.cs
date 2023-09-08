724002366e42d4c5f1c534df7c9c04bc6cfe0626
Gray <azhadolfo1@gmail.com>
2023-09-08 13:38:33 +0800

    Generated gatepass number

---
+++ Models/EmployeeID.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Document_Management.Models
+{
+    public class EmployeeID
+    {
+        [Key]
+        public int Id { get; set; }
+
+      
+        public int Employee_id { get; set; }
+    }
+}
