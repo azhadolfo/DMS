bb8f4bd7a36ee1bf7ad921d074e9f2b2b8c0c8df
azhadolfo <azhadolfo1@gmail.com>
2023-09-12 10:19:10 +0800

    Modified Account/Module and Add Column using migration

---
+++ Migrations/20230912015555_Add new column in Account table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Document_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddnewcolumninAccounttable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Account",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Account");
+        }
+    }
+}
