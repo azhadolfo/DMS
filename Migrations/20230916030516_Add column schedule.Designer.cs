9b2696687ee60ec669335a6ff3ef5ea5ac2b9e81
Gray <azhadolfo1@gmail.com>
2023-09-16 15:01:17 +0800

    fix table and make dropdown in insert module

--- Migrations/20230916030516_Add column schedule.Designer.cs
+++ Migrations/20230916030516_Add column schedule.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Document_Management.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Document_Management.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230916030516_Add column schedule")]
+    partial class Addcolumnschedule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
