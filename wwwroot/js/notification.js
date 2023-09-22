0d568b65c38cf97e5668fdb5f46d69f1dabdc0ad
azhadolfo <azhadolfo1@gmail.com>
2023-09-22 16:52:32 +0800

    implementing signalR

---
+++ wwwroot/js/notification.js
@@ -0,0 +1,40 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/notificationHub").build();
+
+connection.start().then(function () {
+    console.log('connected to hub');
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("OnConnected", function () {
+    OnConnected();
+});
+
+function OnConnected() {
+    var username = $('#hfUsername').val();
+    connection.invoke("SaveUserConnection", username).catch(function (err) {
+        return console.error(err.toString());
+    })
+}
+
+connection.on("ReceivedNotification", function (message) {
+    // alert(message);
+    //DisplayGeneralNotification(message, 'General Message');
+    Swal.fire(
+        'Success',
+        message,
+        'success'
+    )
+});
+
+connection.on("ReceivedPersonalNotification", function (message, username) {
+    // alert(message + ' - ' + username);
+    //DisplayPersonalNotification(message, 'hey ' + username);
+    Swal.fire(
+        'Success',
+        message,
+        'success'
+    )
+});
