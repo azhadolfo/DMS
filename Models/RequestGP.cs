ac9d1d32e7f79e474c1d042c5938cedb42533328
Leonard <andulanleonard@gmail.com>
2023-09-27 17:48:54 +0800

    Update validation in insert javascript change into C#

--- Models/RequestGP.cs
+++ Models/RequestGP.cs
@@ -9,19 +9,26 @@ namespace Document_Management.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(40, MinimumLength = 1, ErrorMessage = "Last Name must be between 1 and 40 characters.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "Invalid characters in Last Name.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; } = null!;
 
         [Required]
+        [StringLength(40, MinimumLength = 1, ErrorMessage = "First Name must be between 1 and 40 characters.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "Invalid characters in First Name.")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; } = null!;
 
         [Required]
+        [StringLength(40, MinimumLength = 1, ErrorMessage = "Last Name must be between 1 and 40 characters.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "Invalid characters in Middle Name.")]
         [Display(Name = "Middle Name")]
         public string MiddleName { get; set; } = null!;
 
         [Required]
-        [IntegerValidator]
+        [Range(1, int.MaxValue, ErrorMessage = "Employee Number must be a positive integer.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Contact Number must be a 10-digit number.")]
         [Display(Name = "Contact")]
         public Int64 Contact { get; set; }
 
@@ -43,7 +50,7 @@ namespace Document_Management.Models
         public string Items { get; set; } = null!;
 
         [Required]
-        [Display(Name = "Schedule Date")]
+        [Display(Name = "Date & Time of Visit")]
         public DateTime Schedule { get; set; }
     }
 }
