7d35532619925ece4e381ced10901659181cedad
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-10-02 11:53:28 +0800

    create enums for constant variable

---
+++ Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿namespace Document_Management.Models.Enums
+{
+    public enum Departments
+    {
+        Accounting,
+        Finance,
+        Marketing,
+        MIS
+    }
+
+    public enum Roles
+    {
+        Admin,
+        User,
+        Validator
+    }
+}
