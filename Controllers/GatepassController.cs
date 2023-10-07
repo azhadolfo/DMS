935d402a9e35478d99c854f916f7fe1644d57db2
Leonard <andulanleonard@gmail.com>
2023-10-07 11:57:44 +0800

    Make approve and disapprove in one module

--- Controllers/GatepassController.cs
+++ Controllers/GatepassController.cs
@@ -108,81 +108,52 @@ namespace Document_Management.Controllers
         }
 
         [HttpPost, ActionName("Approved")]
-        public async Task<IActionResult> Approved(int id)
+        public async Task<IActionResult> Approved(int id, string approvalAction)
         {
             if (_dbcontext.Gatepass == null)
             {
-                return Problem("Entity set 'ApplicationDbContext.Account'  is null.");
-            }
-            var username = HttpContext.Session.GetString("userrole")?.ToLower();
-
-            var client = await _dbcontext.Gatepass.FindAsync(id);
-            //if (client != null)
-            //{
-            //    _dbcontext.Gatepass.Remove(client);
-            //}
-
-            if (client != null)
-            {
-                client.Status = "Approved";
-
-                //Implementing the logs
-                LogsModel logs = new(username, $"Approved Gatepass {client.GatepassId}");
-                _dbcontext.Logs.Add(logs);
-
-                await _dbcontext.SaveChangesAsync();
-                TempData["success"] = "Approved successfully";
-                var hubConnections = _dbcontext.HubConnections.Where(h => h.Username == client.Username).ToList();
-                foreach (var hubConnection in hubConnections)
-                {
-                    await _notificationHub.Clients.Client(hubConnection.ConnectionId).SendAsync("ReceivedPersonalNotification", "Your request has been approved", client.Username);
-                }
-            }
-            return RedirectToAction(nameof(Validator));
-        }
-
-        [HttpGet]
-        public IActionResult Disapproved(int? id)
-        {
-            var requestGP = _dbcontext.Gatepass.FirstOrDefault(x => x.Id == id);
-
-            if (requestGP == null)
-            {
-                return NotFound();
+                return Problem("Entity set 'ApplicationDbContext.Account' is null.");
             }
 
-            return View(requestGP);
-        }
-
-        [HttpPost, ActionName("Disapproved")]
-        public async Task<IActionResult> Disapproved(int id)
-        {
-            if (_dbcontext.Gatepass == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Account'  is null.");
-            }
             var username = HttpContext.Session.GetString("userrole")?.ToLower();
 
             var client = await _dbcontext.Gatepass.FindAsync(id);
 
             if (client != null)
             {
-                client.Status = "Disapproved";
-
-                //Implementing the logs
-                LogsModel logs = new(username, $"Disapproved Gatepass {client.GatepassId}");
-                _dbcontext.Logs.Add(logs);
-
-                await _dbcontext.SaveChangesAsync();
-                TempData["success"] = "Disapproved successfully";
-                var hubConnections = _dbcontext.HubConnections.Where(h => h.Username == client.Username).ToList();
-                foreach (var hubConnection in hubConnections)
+                if (approvalAction == "Confirm Approve")
                 {
-                    await _notificationHub.Clients.Client(hubConnection.ConnectionId).SendAsync("ReceivedPersonalNotification", "Your request has been disapproved", client.Username);
+                    // Approve logic
+                    client.Status = "Approved";
+                    LogsModel logs = new(username, $"Approved Gatepass {client.GatepassId}");
+                    _dbcontext.Logs.Add(logs);
+                    await _dbcontext.SaveChangesAsync();
+                    TempData["success"] = "Approved successfully";
+                    var hubConnections = _dbcontext.HubConnections.Where(h => h.Username == client.Username).ToList();
+                    foreach (var hubConnection in hubConnections)
+                    {
+                        await _notificationHub.Clients.Client(hubConnection.ConnectionId).SendAsync("ReceivedPersonalNotification", "Your request has been approved", client.Username);
+                    }
+                }
+                else if (approvalAction == "Confirm Disapprove")
+                {
+                    // Disapprove logic
+                    client.Status = "Disapproved";
+                    LogsModel logs = new(username, $"Disapproved Gatepass {client.GatepassId}");
+                    _dbcontext.Logs.Add(logs);
+                    await _dbcontext.SaveChangesAsync();
+                    TempData["success"] = "Disapproved successfully";
+                    var hubConnections = _dbcontext.HubConnections.Where(h => h.Username == client.Username).ToList();
+                    foreach (var hubConnection in hubConnections)
+                    {
+                        await _notificationHub.Clients.Client(hubConnection.ConnectionId).SendAsync("ReceivedPersonalNotification", "Your request has been disapproved", client.Username);
+                    }
                 }
             }
+
             return RedirectToAction(nameof(Validator));
         }
+
 
         [HttpGet]
         public IActionResult RecievedGP()
