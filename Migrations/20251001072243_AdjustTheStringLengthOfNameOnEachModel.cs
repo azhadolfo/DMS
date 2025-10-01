ecc7a1e009da51f5618996518e49ff46b7dfda6f
azhadolfo <azhadolfo1@gmail.com>
2025-10-01 15:23:47 +0800

    fix: adjust the string length of each select list v2

---
+++ Migrations/20251001072243_AdjustTheStringLengthOfNameOnEachModel.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Document_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustTheStringLengthOfNameOnEachModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SubCategoryName",
+                table: "SubCategories",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DepartmentName",
+                table: "Departments",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Companies",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryName",
+                table: "Categories",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SubCategoryName",
+                table: "SubCategories",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DepartmentName",
+                table: "Departments",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Companies",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryName",
+                table: "Categories",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+    }
+}
