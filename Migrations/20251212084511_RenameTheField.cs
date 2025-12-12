2371af5d686687cf1a5570b09c38600da94d8ceb
azhadolfo <azhadolfo1@gmail.com>
2025-12-12 16:46:12 +0800

    fix: rename the submitted date to date submitted

---
+++ Migrations/20251212084511_RenameTheField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Document_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTheField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubmittedDate",
+                table: "FileDocuments",
+                newName: "DateSubmitted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateSubmitted",
+                table: "FileDocuments",
+                newName: "SubmittedDate");
+        }
+    }
+}
