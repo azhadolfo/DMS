babb136ea4ca66a715c7395357d2bf1892e806cb
azhadolfo <azhadolfo1@gmail.com>
2026-04-13 10:33:10 +0800

    fix: adjust the limiter of reading ocr

--- Program.cs
+++ Program.cs
@@ -102,7 +102,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-// This code is to change the behaviour of timestamp of postgresql
+// This code is to change the behavior of timestamp of postgresql
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 app.UseMiddleware<MaintenanceMiddleware>();
